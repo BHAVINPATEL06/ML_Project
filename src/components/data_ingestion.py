56731abac4f956166155eb28833101950c916332
Bhavin Patel <bhavinpatel242846@gmail.com>
2025-12-31 19:15:11 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
+
 
 ## Defining DataIngestionConfig Class: This class is created using the @dataclass decorator to define class variables, which act as inputs for the DataIngestion component.
 ## These inputs specify where to save the training data, test data, and raw data paths.
@@ -60,4 +63,7 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr ,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
